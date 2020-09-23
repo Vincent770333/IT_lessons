bde69e969436aa1f7d357284979f0c5135b09904
Vincent770333 <66562736+Vincent770333@users.noreply.github.com>
2020-09-23 11:34:28 +0300

    Create Task2.java

---
+++ 23.09/Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+    public static void main(String[] args) {
+        for(int i='A'; i<='Z';i++){
+            System.out.print((char)i);
+        }
+        System.out.println();
+        for(int i='a'; i<='z';i++){
+            System.out.print((char)i);
+        }
+    }
+}
